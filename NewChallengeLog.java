1075ecca6fd31b1d36289f97cfa6d9264c2e560d
Andy Simmons <rocknswimmer116@gmail.com>
2023-08-11 11:51:56 -0400

    hacker earths user unfreindly setup cost too much time

--- NewChallengeLog.java
+++ NewChallengeLog.java
@@ -1,2 +1,3 @@
 //new playground new log
 
+//8/11 another hiring event on hackerEarth where I ran out of time thanks to needing to spend more than 30 minutes extracting and testing the extraction of the test information from a document, since they waste users time by not having a proper test environment that can run the function being tested by inputing varibles and running multiple sites. could have also finished in time if not translating javascript to java.
