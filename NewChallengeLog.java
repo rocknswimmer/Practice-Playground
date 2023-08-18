d6aec9e9f2eb986557ac9edba142668dc97497c5
Andy Simmons <rocknswimmer116@gmail.com>
2023-08-18 14:34:40 -0400

    august circuit start

--- NewChallengeLog.java
+++ NewChallengeLog.java
@@ -3,3 +3,5 @@
 //8/11 another hiring event on hackerEarth where I ran out of time thanks to needing to spend more than 30 minutes extracting and testing the extraction of the test information from a document, since they waste users time by not having a proper test environment that can run the function being tested by inputing varibles and running multiple sites. could have also finished in time if not translating javascript to java.
 
 //8/17 remo confusing take another hacker rank but then try to test on site. learned about python lambda functions.
+
+//8/18 August Circuit start
