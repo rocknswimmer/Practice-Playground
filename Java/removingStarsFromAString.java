7e11794cb2c6d0a9aa6ad9e3795cb0f8ec9638b2
Andy Simmons <rocknswimmer116@gmail.com>
2023-11-13 11:45:27 -0500

    compairing faster solutions

--- Java/removingStarsFromAString.java
+++ Java/removingStarsFromAString.java
@@ -30,3 +30,53 @@ class Solution {
         return combined;
     }
 }
+
+// most hidden opperations solution
+// class Solution {
+//         public String removeStars(String s) {
+//         StringBuilder res = new StringBuilder();
+//         for (char c : s.toCharArray())
+//             if (c == '*')
+//                 res.setLength(res.length() - 1);
+//             else
+//                 res.append(c);
+//         return res.toString();
+//     }
+// }
+
+//simple solution/trick to the problem without fancy language functionality
+// class Solution {
+//     public String removeStars(String s) {
+//         String res = "";
+
+//         // Initilizing the count of stars
+//         int count = 0;
+
+//         // Traversing backwards for the ease removal of closest non-star character
+//         for(int i = s.length()-1; i >= 0; i--)
+//         {
+//             if(s.charAt(i) != '*' && count == 0)
+//             {
+//                 res += s.charAt(i);
+//             }
+//             if(s.charAt(i) == '*')
+//             {
+//                 count++;
+//             }
+//             else if(s.charAt(i) != '*' && count != 0)
+//             {
+//                 count--;
+//             }
+//         }
+
+//         // Reversing the string
+//         String new_str = "";
+//         for(int i = 0; i < res.length(); i++)
+//         {
+//             char ch = res.charAt(i);
+//             new_str= ch + new_str;
+//         }
+
+//         return new_str;
+//     }
+// }
