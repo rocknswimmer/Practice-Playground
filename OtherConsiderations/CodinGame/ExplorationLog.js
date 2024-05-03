312e87b51989c32b744421f23a1b5933c7e73a56
Andy Simmons <rocknswimmer116@gmail.com>
2024-05-03 11:33:29 -0400

    more clash

--- OtherConsiderations/CodinGame/ExplorationLog.js
+++ OtherConsiderations/CodinGame/ExplorationLog.js
@@ -1,3 +1,5 @@
 //5/1/24 leveling up while exploring the different paths started with algorithms path, and am getting familiar with how the site works. a bit more intro before the clash yest would have helped, but I came close and now know that muscle has gotten weak.
 
 //5/2 lot more clash matches today to practice, don't like when it ends on you and you loose code/environment, have been 2 mins away several times
+
+//5/3 ran a few more clashes for int prep/ calm nerves not sure if just getting easier ones, or if the muscle is starting to strengthen
