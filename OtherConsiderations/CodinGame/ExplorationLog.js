b0bc86af3ae271f15a4a0650d0651fb5ebcea9e9
Andy Simmons <rocknswimmer116@gmail.com>
2024-05-05 15:49:46 -0400

    learning

--- OtherConsiderations/CodinGame/ExplorationLog.js
+++ OtherConsiderations/CodinGame/ExplorationLog.js
@@ -2,4 +2,6 @@
 
 //5/2 lot more clash matches today to practice, don't like when it ends on you and you loose code/environment, have been 2 mins away several times
 
-//5/3 ran a few more clashes for int prep/ calm nerves not sure if just getting easier ones, or if the muscle is starting to strengthen
+//5/3 ran a few more clashes for int prep/ calm nerves not sure if just getting easier ones, or if the muscle is starting to strengthen the following is the most rediculous fewest bits clash code, tells how many socks needed to guaruntee a matching pair, print(+readline()+1)
+
+//5/5 imporved old golfs with what I have learned to climb rankings, still want to know the super tricks for halving my code again
