33d583c0d4adc9ceab6d7cff4aa5283453ed6178
Andy Simmons <rocknswimmer116@gmail.com>
2024-05-12 20:37:45 -0400

    skipped level 11

--- OtherConsiderations/CodinGame/ExplorationLog.js
+++ OtherConsiderations/CodinGame/ExplorationLog.js
@@ -10,3 +10,4 @@
 
 //5/10 landed everywhere but highground
 //5/11 reached disiple level/ top 10000
+//5/12 broke top 1000 in clash, so finished 3 challenges the last 500 pointer jumped me from lvl 10 to 12
