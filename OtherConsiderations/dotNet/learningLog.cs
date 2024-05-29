4ba62f6626124e9fe97442d77ba42216a7ce763d
Andy Simmons <rocknswimmer116@gmail.com>
2024-05-29 13:13:03 -0400

    \t does not add a tab in c#

--- OtherConsiderations/dotNet/learningLog.cs
+++ OtherConsiderations/dotNet/learningLog.cs
@@ -1,1 +1,4 @@
 // 5/28 learning basics like comments the same as Java based languages, var implies type, not sure if should be used though, lesson still recomends using type to help with intention
+// casting is opposite of most other languages, (decimal)number instead of decimal(number)
+
+//5/29 continuing the course, basic grade print, learned \t does not add a tab, but moves to the next "tab spot" could only add a space if 3/4 of the way through the tab space when added. Seems like a real design flaw.
