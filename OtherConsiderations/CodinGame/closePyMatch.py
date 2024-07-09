b62d45a4ba512356bec5ae389ff1d351799facc8
Andy Simmons <rocknswimmer116@gmail.com>
2024-07-09 16:51:04 -0400

    found the i char dif, but still lost on time and lang

--- OtherConsiderations/CodinGame/closePyMatch.py
+++ OtherConsiderations/CodinGame/closePyMatch.py
@@ -7,3 +7,7 @@ for x in range(n):print('#'*n if x==0 or x==n-1 else'#'+(' '*(n-2))+'#')
 
 n=int(input())
 for i in range(n):print(n*"#"if (i+1)%n<2 else"#"+" "*(n-2)+"#")
+
+
+print(sum(int(c) for c in bin(int(input()))[2:]))
+print(sum(int(x)for x in bin(int(input()))[2:]))
