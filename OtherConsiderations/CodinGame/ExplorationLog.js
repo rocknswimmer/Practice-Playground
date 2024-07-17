c0933c6dab32ab86c8c32bf4d0594cfac81f5bef
Andy Simmons <rocknswimmer116@gmail.com>
2024-07-17 13:53:46 -0400

    clash

--- OtherConsiderations/CodinGame/ExplorationLog.js
+++ OtherConsiderations/CodinGame/ExplorationLog.js
@@ -48,4 +48,4 @@
 
 //7/13 just learned that -0 exists in programing, not sure how I have not encountered this before
 
-
+//7/17 jumped all the way back from 500s to 300s still haven't broken into the top 300
